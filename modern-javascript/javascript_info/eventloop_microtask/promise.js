b8b2dd0276bc6f5806af72423b4b9c296e44ae0b
Minsung Kim <epicblue@hanmail.net>
2022-01-31 18:00:02 +0900

    정처기 4장 마무리 & 모던 자바스크립트 이벤트 루프(Macrotask, Microtask)

---
+++ modern-javascript/javascript_info/eventloop_microtask/promise.js
@@ -0,0 +1,17 @@
+// 그 동안 잘못 알고 있었던 것
+// Promise Executor도 microtask에 속하기 때문에
+// 메인 컨텍스트가 끝나고 나서야 작동하는 것인 줄 알았다.
+// 렌더링 보다 우선시 된다.
+
+setTimeout(() => console.log("SetTimeout  Macrotask")); //5
+
+console.log("메인 스택 Promise 생성 전"); // 1
+
+new Promise((resolve) => {
+  console.log("promise executor 동기적"); // 3
+  resolve();
+}).then(() => {
+  console.log("Promise Handler Microtask"); // 4
+});
+
+console.log("메인 스택 Promise 생성 후"); // 2
